148f16b7e5e75e7ae2f3bfec5c745df89ec9c4c3
Valentina Álvarez <valentinaav825@gmail.com>
2025-03-04 12:49:33 -0500

    Primer commit

---
+++ src/main/java/com/example/RedInmobiliaria/RedInmobiliariaApplication.java
@@ -0,0 +1,13 @@
+package com.example.RedInmobiliaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedInmobiliariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedInmobiliariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RedInmobiliaria/RedInmobiliariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RedInmobiliaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedInmobiliariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
