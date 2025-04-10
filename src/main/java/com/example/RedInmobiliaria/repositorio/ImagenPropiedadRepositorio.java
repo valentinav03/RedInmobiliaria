41079a00d0e400e65b375d5d74a0c3f2fb89e956
NelsonMendoza1021 <nelsondavid1021@gmail.com>
2025-04-10 05:34:06 -0500

    Implementacion de Modelo, Servicio, Controlador, Repositorio del CRUD de ImagenesPropiedad

---
+++ src/main/java/com/example/RedInmobiliaria/repositorio/ImagenPropiedadRepositorio.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.RedInmobiliaria.repositorio;
+
+import com.example.RedInmobiliaria.modelo.ImagenPropiedad;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ *
+ * @author Nelson
+ */
+@Repository
+public interface ImagenPropiedadRepositorio extends JpaRepository<ImagenPropiedad, Integer> {
+    List<ImagenPropiedad> findByPropiedadId(Integer idPropiedad);
+}
+
